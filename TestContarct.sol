467b084f2b8852eeb7e03afe2ed4869d27dc3fa6
Yaroslav Prokhorovych <93757015+LifeOfPunk@users.noreply.github.com>
2022-04-26 18:07:47 +0800

    Update TestContarct.sol

--- TestContarct.sol
+++ TestContarct.sol
@@ -2,10 +2,22 @@
 
 pragma solidity ^0.8.0;
 
-contract DemoTrasnfer {
+contract DemoAddress {
+    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
+    function receiveFunds () public payable {
+    }
+
+    function getBalance (address targetAddr) public view returns (uint) {
+       return targetAddr.balance;
+    }
+
+    function transferTo (address targetAddr,uint amount) public {
+        address payable _to = payable (targetAddr);
+        _to.transfer(amount); 
+
+    }
+
+    
 
 }
-
-
-
